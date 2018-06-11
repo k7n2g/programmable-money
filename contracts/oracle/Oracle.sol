169f7c4c9aa6496260a1dc8a565d33987098efdc
Andrei Karpushonak <karpushonak@gmail.com>
2018-06-11 18:12:46 +0200

    Use openzeppelin-solidity as a dependency

--- contracts/oracle/Oracle.sol
+++ contracts/oracle/Oracle.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.4.23;
 
-import "../math/SafeMath.sol";
-import "../ownership/Ownable.sol";
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 
 
 /**
